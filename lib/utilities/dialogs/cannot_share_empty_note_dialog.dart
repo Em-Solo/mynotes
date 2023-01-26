a82d77be00e841c29105265714500ef769439b63
Em-Solo <solomonidese2001@gmail.com>
2023-01-26 18:34:27 +0000

    step-21

---
+++ lib/utilities/dialogs/cannot_share_empty_note_dialog.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+
+Future<void> showCannotShareEmptyNoteDialog(BuildContext context) {
+  return showGenericDialog<void>(
+      context: context,
+      title: "Sharing",
+      content: "You cannot share an empty note!",
+      optionsBuilder: () => {
+            'OK': null,
+          });
+}
